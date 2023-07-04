b6c66478076ef05efa42ae9709b766a884803852
draconian0428 <jakus.superdev@gmail.com>
2023-07-05 09:18:42 +0930

    step2 done

--- server.js
+++ server.js
@@ -2,39 +2,90 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const path = require('path');
+const mysql = require('mysql');
 
 const app = express();
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'image',
+});
 
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 
 app.post('/getImg', (req, res) => {
-
   try {
     const imageBase64 = req.body.file;
     const filename = req.body.fileName;
-    const matches = imageBase64.match(/^data:image\/([a-z]+);base64,(.+)$/i);
 
-    if (!matches || matches.length !== 3) {
-      throw new Error('Invalid base64 string');
-    }
-
-    const [, extension, data] = matches;
-
-    const buffer = Buffer.from(data, 'base64');
-    const filepath = `./imgs/${filename}`;
-
-    fs.writeFileSync(filepath, buffer);
-
-    console.log(`Image saved as ${filepath}`);
-    res.send({ok: true})
+    pool.query(`INSERT INTO USER_IMAGES (iName, base64) VALUES ("${filename}", '${imageBase64}')`, (err, result) => {
+      if (err) {
+        throw err
+      } else {
+        console.log('Successed!');
+        res.send({ ok: true });
+      }
+    })
 
   } catch (err) {
     console.log(err)
-    res.send({ok: false, ...err});
+    res.send({
+      ok: false,
+      result: err
+    });
   }
+});
+
+app.get('/imageslist', (req, res) => {
+  pool.query(`SELECT iId, iName FROM user_images`, (err, result) => {
+    if (err) {
+      res.send({
+        ok: false,
+        result: err
+      })
+    }
+
+    res.send({
+      ok: true,
+      result
+    })
+  });
 })
+
+app.get('/images/:fileId', (req, res) => {
+  const uId = req.params.fileId;
+  pool.query(`SELECT * FROM user_images where iId=${uId}`, (err, result) => {
+    if (err) {
+      res.send({
+        ok: false,
+        result: err
+      })
+    }
+    console.log(result);
+    res.send({
+      ok: true,
+      result
+    })
+  })
+});
+
+
+
+// Test the connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the database!');
+    connection.release(); // Release the connection
+  }
+});
+
 
 const port = process.env.PORT || 443;
 
