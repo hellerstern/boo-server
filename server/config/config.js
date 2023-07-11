d6ab33f42595cb7d6c802a051c4a78e51d4273c3
draconian0428 <jakus.superdev@gmail.com>
2023-07-11 16:56:12 +0900

    api-key generation done

--- server/config/config.js
+++ server/config/config.js
@@ -11,12 +11,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 // ============================
 //  Authentication seed
 // ============================
-process.env.SEED = process.env.SEED || "dev-secret-seed";
+process.env.SEED = process.env.SEED || "Boo";
 
 // ============================
 //  Data base
 // ============================
-process.env.DBHOST = process.env.DBHOST || "174.138.188.39";
+process.env.DBHOST = process.env.DBHOST || "localhost";
 process.env.DBUSER = process.env.DBUSER || "root";
 process.env.DBPASSWORD = process.env.DBPASSWORD || "";
-process.env.DBNAME = process.env.DBNAME || "image";
+process.env.DBNAME = process.env.DBNAME || "image"; 
